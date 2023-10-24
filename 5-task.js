fa135b9952cc89b126fbc86333a8edbd322ebfd1
Durbekjon <Saydaliyevdurbek0512@gmail.com>
2023-10-24 11:03:17 +0500

    5-task

---
+++ 5-task.js
@@ -0,0 +1,16 @@
+function numbers(a, b, c, d) {
+  if (a > 0 && b > 0 && c > 0 && d > 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+let a = 3
+let b = -5
+let c = 2
+let d = 8
+
+let natija = numbers(a, b, c, d)
+
+console.log(natija)
