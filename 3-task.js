8ce8c777f41def4102441f90985a33ba8c881024
Durbekjon <Saydaliyevdurbek0512@gmail.com>
2023-10-24 10:58:11 +0500

    3-task

---
+++ 3-task.js
@@ -0,0 +1,17 @@
+function check(yil) {
+  if ((yil % 4 == 0 && yil % 100 != 0) || yil % 400 == 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// Misol
+let year = 2028 // Yilni o'zgartiring
+let kabisa = check(year)
+
+if (kabisa) {
+  console.log(year + '-yil kabisa yili')
+} else {
+  console.log(year + '-yil kabisa yili emas')
+}
