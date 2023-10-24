63da95ebdd45dfa797bea43e9fb27dc4cf5e5df2
Durbekjon <Saydaliyevdurbek0512@gmail.com>
2023-10-24 11:15:06 +0500

    11-task

---
+++ 11-task.js
@@ -0,0 +1,6 @@
+let originalArray = [1, 2, 3, 4, 5]
+
+let reversedArray = originalArray.slice().reverse()
+
+console.log('Original Array: ' + originalArray)
+console.log('Reversed Array: ' + reversedArray)
